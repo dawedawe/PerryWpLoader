e87690e54d8ba5affcc8d01acd4d871498dcf081
dawe <dawedawe@posteo.de>
2023-09-16 22:53:21 +0200

    baic implementation for downloads

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified MyLib (downloadAll)
 
-main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  MyLib.downloadAll
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,32 @@
-module MyLib (someFunc) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module MyLib (downloadAll) where
+
+import Control.Exception as E
+import Control.Lens
+import qualified Data.ByteString.Lazy as B
+import qualified Network.HTTP.Client as HTTPClient
+import Network.Wreq
+
+tryGet url =
+  do
+    r <- get url
+    return $ Just $ r ^. Network.Wreq.responseBody
+    `E.catch` \(e :: HTTPClient.HttpException) -> return Nothing
+
+downloadAll =
+  go 1
+  where
+    go n =
+      do
+        let filename = "neo" ++ show n ++ "_1920x1080_0.jpg"
+        let u = "https://perry-rhodan.net/sites/default/files/downloads/" ++ filename
+        r <- tryGet u
+        case r of
+          Nothing -> putStrLn $ "download failed for n = " ++ show n ++ "\nStopping"
+          Just body ->
+            do
+              action <- B.writeFile filename body
+              putStrLn $ "downloaded " ++ filename
+              go (succ n)
